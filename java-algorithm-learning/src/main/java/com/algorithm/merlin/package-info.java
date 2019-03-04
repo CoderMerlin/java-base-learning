94de339e8a88bf465a16d1698f3db2aef460e73e
huangmai <huangmai@inner.sss.com>
2019-03-04 14:18:52 +0800

    冒泡排序/插入排序/选择排序/递归

--- java-algorithm-learning/src/main/java/com/algorithm/merlin/package-info.java
+++ java-algorithm-learning/src/main/java/com/algorithm/merlin/package-info.java
@@ -1,7 +1,7 @@
 /**
  * @Title: package-info
  * @ProjectName java-base-learning
- * @Description: 主要学习java算法
+ * @Description: 主要学习java算法 ，参考网上相关博客或视频。
  * @author merlin
  * @date 2019/2/2820:16
  */
